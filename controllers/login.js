a3e71c28a2861d3d92cbcc01e77758c7098a19f5
Rolopens <rolopens@gmail.com>
2020-03-06 01:34:40 +0800

    I want to kill myself

---
+++ controllers/login.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports.controller = function (app) {
+
+    app.get('/login', function(req, res) {
+        if (!req.session.uid) res.redirect("/");
+        else res.redirect("/preacts");
+    })
+
+    app.post('/login', function(req, res) {
+    	// put logic here on how to login
+
+    	res.render('homepageTutor')
+
+
+        // if(req.session.uid != null) {
+        //     return res.redirect('/')
+        // }
+
+        // var email = req.body.email;
+        // var password = req.body.password;
+
+        // if (email == "" || email == undefined || password == "" || password == undefined)
+        //     return res.redirect("/login?error=badCredentials")
+        // else {
+        //     authService.checkUser(email, password).then((result)=>{
+        //         req.session.uid = result._id;
+        //         //console.log(req.session.uid);
+        //         return res.redirect('/home');
+        //     }).catch((err)=>{
+        //         return res.redirect("/login?error=BadCredentials");
+        //     })
+        // }
+    })
+
+    app.get('/login', function(req, res) {
+    	// put logic here on how to login
+    	res.render('homepageTutor')
+    })
+   
+}
